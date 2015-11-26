40704109a243df227784ecb329bb6902f701acdd
Stefan Marr <git@stefan-marr.de>
2015-11-26 11:06:04 +0100

    Update script can take now a desired hg id as argument
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- update-graal-revision.sh
+++ update-graal-revision.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-cd graal-compiler
-hg pull
-hg update
-hg id -i > ../graal.revision
+### usage: ./update-graal-revision.sh [hg-id]
+
+if [ -z "$1" ]; then
+  # no argument given, update to latest version
+  cd graal-compiler
+  hg pull
+  hg update
+  hg id -i > ../graal.revision
+  HG_ID=`hg id -i`
+  cd ..
+else
+  echo "$1\n" > graal.revision
+  HG_ID="$1"
+fi
+git commit -m "Updated graal.revision to ${HG_ID}" graal.revision
