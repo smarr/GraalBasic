bbc636b95de60da6139f82b9d81071fba736a38f
Stefan Marr <git@stefan-marr.de>
2016-08-05 18:10:22 +0200

    Updated build.sh
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- build.sh
+++ build.sh
@@ -5,32 +5,30 @@ echo %% Make sure git submodules are up-to-date
 echo ""
 git submodule update --init --recursive
 
-if [ -d "truffle" ]; then
-  cd truffle
-  ../mx/mx clean
-  cd ..
-fi
+cd truffle
+../mx/mx clean
+cd ..
 
-if [ -d "jvmci" ]; then
-  cd jvmci
-  ../mx/mx clean
-  cd ..
-fi
+cd graal-jvmci-8
+../mx/mx clean
+export JAVA_HOME=`../mx/mx jdkhome`
+cd ..
 
-cd graal-compiler
-../mx/mx sforceimports
+cd graal-core
 
 echo ""
 echo %% Build Graal Compiler
 echo ""
-../mx/mx --vm server clean
-../mx/mx --vm server build
+../mx/mx clean
+../mx/mx build
 
+echo ""
+echo %% Deploy Graal Into JVMCI Built
+cp mxbuild/dists/graal.jar $JAVA_HOME/jre/lib/jvmci/
 
 if [ -d ~/.local ]; then
-  JAVA_BUILD=`../mx/mx jdkhome`
   if [ -d ~/.local/graal-core ]; then
     rm -Rf ~/.local/graal-core
   fi
-  mv $JAVA_BUILD ~/.local/graal-core
+  mv $JAVA_HOME ~/.local/graal-core
 fi
