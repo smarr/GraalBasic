fbb3a1ab4f30056bc55e4b37850167d978daf1e2
Stefan Marr <git@stefan-marr.de>
2018-07-21 11:54:07 +0100

    Simplify build.sh by using makegraaljdk command
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-## We got issues with the version check when building from git, so, disable it
-export JVMCI_VERSION_CHECK=ignore
-
 echo ""
 echo %% Make sure git submodules are up-to-date
 echo ""
@@ -24,24 +21,14 @@ echo %% JAVA_HOME=$JAVA_HOME
 echo %% Build Graal Compiler
 echo ""
 cd truffle/compiler
+## We got issues with the version check when building from git, so, disable it
+export JVMCI_VERSION_CHECK=ignore
 ../../mx/mx clean
 ../../mx/mx build
-
-echo ""
-echo %% Deploy Graal Into JVMCI Built
-cp mxbuild/dists/graal.jar $JAVA_HOME/jre/lib/jvmci/
-
-cd ../sdk
-../../mx/mx clean
-../../mx/mx build
-
-echo ""
-echo %% Deploy Graal SDK Into JVMCI Built
-cp mxbuild/dists/graal-sdk.jar $JAVA_HOME/jre/lib/jvmci/
 
 if [ -d ~/.local ]; then
   if [ -d ~/.local/graal-core ]; then
     rm -Rf ~/.local/graal-core
   fi
-  mv $JAVA_HOME ~/.local/graal-core
+  ../../mx/mx makegraaljdk ~/.local/graal-core
 fi
